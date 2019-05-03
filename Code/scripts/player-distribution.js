e890fa117795f6bc70ee57d11b99bbbc7fc71579
Adam Radvan <xradvan@fi.muni.cz>
2019-05-03 10:17:12 +0200

    adding href, working on name input check

--- Code/scripts/player-distribution.js
+++ Code/scripts/player-distribution.js
@@ -3,14 +3,16 @@
 // Function that is called from HTML body onload
 // Dynamically creates divs depending on data from sessionStorage, which is used on page before: 'team-count'
 function createTeamContainers() {
+    let teamCount = 2;
 
-    if (sessionStorage.getItem('teamCount') === null) {
-        teamCount = 2;
-    } else {
-        teamCount = sessionStorage.getItem('teamCount');
+    if (sessionStorage.getItem('teamCount') !== null) {
+        teamCount = parseInt(sessionStorage.getItem('teamCount'), 10);
     }
 
     const teamSectionContainer = document.getElementsByClassName('teams-section-container')[0];
+    while (teamSectionContainer.firstChild) {
+        teamSectionContainer.removeChild(teamSectionContainer.firstChild);
+    }
 
     for (i = 1; i < teamCount + 1; i++) {
         let teamContainerDiv = document.createElement('div');
@@ -20,6 +22,7 @@ function createTeamContainers() {
         teamHeader.className = 'team-number-header';
         teamHeader.innerHTML = `TEAM ${i}`;
         teamContainerDiv.appendChild(teamHeader);
+
         for (j = 1; j < 3; j++) {
             let playerNameInput = document.createElement('input');
             playerNameInput.className = `player-name-container player${j}`;
@@ -33,9 +36,28 @@ function createTeamContainers() {
             teamContainerDiv.appendChild(playerNameInput);
         }
         teamSectionContainer.appendChild(teamContainerDiv);
-
     }
 }
 
+function checkValidNameInputs() {
+    const playerNameInputs = [...document.getElementsByClassName('player-name-container')];
+    playerNameInputs.forEach(input => {
+        if (input.value) {
+            console.log(input.value);
+        } else {
+            alert('All names must be filled!');
+            return;
+        }
+    })
+    // return true;
+}
 
-//  TODO:       Check for valid input upon submit, add warnign that all names have to added
+//  TODO: Check for valid input upon submit, add warning that all names have to be added
+const submitBtn = document.getElementsByClassName('submit-btn')[0];
+submitBtn.addEventListener('click', () => {
+    checkValidNameInputs();
+    console.log('All valid');
+    // sessionStorage.setItem('teamCount', String(teamCountInputNode.value));
+    // console.log('current teamcount in storage: ' + sessionStorage.getItem('teamCount'));
+
+})
